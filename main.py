835d6394f0d4a6f8ee0830fc450f87a4036262e9
amosborne <7675373+amosborne@users.noreploy.github.com>
2020-10-16 21:55:06 -0700

    load skin and display

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel
+from PyQt5.QtGui import QPixmap
+from PyQt5.QtCore import QRect
+from enum import IntEnum
+
+
+class Puyo(IntEnum):
+    GARBAGE = 0
+    RED = 1
+    GREEN = 2
+    BLUE = 3
+    YELLOW = 4
+    PURPLE = 5
+
+
+def puyoPixmap(puyo, skin_pixmap):
+    area = QRect(puyo * 64, 7 * 32, 64, 64)
+    return skin_pixmap.copy(area)
 
 
 def main():
@@ -8,6 +25,13 @@ def main():
 
     w = QMainWindow()
     w.setWindowTitle("Puyo Trainer")
+
+    skin_pixmap = QPixmap("ppvs2_skins/gummy.png")
+    label = QLabel(w)
+    puyo_pixmap = puyoPixmap(Puyo.YELLOW, skin_pixmap)
+    label.setPixmap(puyo_pixmap)
+    w.setCentralWidget(label)
+
     w.show()
 
     sys.exit(app.exec_())
