3c512ad3695aa557cf03256bbe131606a8603f22
amosborne <7675373+amosborne@users.noreploy.github.com>
2020-10-24 14:44:36 -0700

    new grid model class

---
+++ src/puyolib/puyogridmodel.py
@@ -0,0 +1,82 @@
+import numpy as np
+from copy import deepcopy
+from pandas import DataFrame
+from puyomodel import Puyo, Direc
+
+
+# A class structure for controlling and interrogating a grid of puyos.
+class AbstractPuyoGridModel:
+    def __init__(self, size, nhide):
+        fullsize = (size[0] + nhide, size[1])
+        self.board = np.full(fullsize, Puyo.NONE).astype(Puyo)
+        self.nhide = nhide
+
+    def __iter__(self):
+        return ((pos, puyo) for (pos, puyo) in np.ndenumerate(self.board))
+
+    def __getitem__(self, key):
+        return self.board[key]
+
+    def __setitem__(self, key, value):
+        self.board[key] = value
+
+    def __str__(self):
+        board_flipped = np.flipud(self.board)
+        height, width = board_flipped.shape
+
+        col_names = ["c" + str(i + 1) for i in range(width)]
+        row_names1 = ["r" + str(i + 1) for i in range(height - self.nhide)]
+        row_names2 = ["h" + str(i + 1) for i in range(self.nhide)]
+
+        dataframe = DataFrame(board_flipped)
+        dataframe.columns = col_names
+        dataframe.index = row_names2 + list(reversed(row_names1))
+
+        return dataframe.__str__()
+
+    # Subtraction of two puyo grid models (assuming the same full size)
+    # returns the set of (pos, puyo) for which self has a puyo and other
+    # does not. This is primarily intended for determining ghost puyo
+    # locations given a post-move and pre-move puyo board model.
+    def __sub__(self, other):
+        delta = set()
+
+        for pos, puyo in self:
+            other_puyo = other[pos]
+            if puyo is not Puyo.NONE and other_puyo is Puyo.NONE:
+                delta.add((pos, puyo))
+
+        return delta
+
+    def copy(self):
+        return deepcopy(self)
+
+    def isHidden(self, key):
+        return key[0] >= self.board.shape[0] - self.nhide - 1
+
+    def getAdjacent(self, key):
+        adj = set()
+
+        for pos, puyo in self:
+            if puyo is not Puyo.NONE and adjacency_direction(key, pos):
+                adj.add((pos, puyo))
+
+        return adj
+
+
+class PuyoBoardModel(AbstractPuyoGridModel):
+    def applyMove(move):
+        pass
+
+
+def adjacency_direction(pos1, pos2):
+    if pos1[1] == pos2[1] and pos1[0] + 1 == pos2[0]:
+        return Direc.NORTH
+    elif pos1[1] == pos2[1] and pos1[0] - 1 == pos2[0]:
+        return Direc.SOUTH
+    elif pos1[0] == pos2[0] and pos1[1] + 1 == pos2[1]:
+        return Direc.EAST
+    elif pos1[0] == pos2[0] and pos1[1] - 1 == pos2[1]:
+        return Direc.WEST
+    else:
+        return None
